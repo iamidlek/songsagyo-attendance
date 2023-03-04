7d1c44ad95d4ce0aa5e603adcdd1ef39c980a933
iamidlek <iamidlek@naver.com>
2023-03-05 00:40:41 +0900

    dotted style test

--- src/pages/WeeklyTracePage.tsx
+++ src/pages/WeeklyTracePage.tsx
@@ -35,37 +35,41 @@ export const WeeklyTracePage: React.FC<WeeklyTracePageProps> = ({
     <div className="fortest relative ">
       <div className="flex gap-2 justify-around">
         <div className="w-1/2 bg-gradient-to-r from-amber-400 to-amber-500  rounded-lg py-3 px-2 font-bold">
-          <div className="border-dotted border-[0.8rem] border-white mx-auto flex flex-wrap max-w-[17.8rem] sm:max-w-[80rem] pt-3 pb-8 gap-y-8 justify-around items-center">
-            {march.map((date) => {
-              if (!attendInfo) return;
-              const time = attendInfo[date];
-              return (
-                <AttendCircle
-                  key={date}
-                  color="border-amber-300 bg-orange-100/90"
-                  date={date}
-                  time={time}
-                  attend={Boolean(time)}
-                />
-              );
-            })}
+          <div className="path">
+            <div className="mx-auto flex flex-wrap max-w-[17.8rem] sm:max-w-[80rem] pt-3 pb-8 gap-y-8 justify-around items-center">
+              {march.map((date) => {
+                if (!attendInfo) return;
+                const time = attendInfo[date];
+                return (
+                  <AttendCircle
+                    key={date}
+                    color="border-amber-300 bg-orange-100/90"
+                    date={date}
+                    time={time}
+                    attend={Boolean(time)}
+                  />
+                );
+              })}
+            </div>
           </div>
         </div>
         <div className="w-1/2 bg-gradient-to-r from-red-200 to-pink-300 rounded-lg py-3 px-2 font-bold">
-          <div className="border-dotted border-[0.8rem] border-white mx-auto flex flex-wrap max-w-[17.8rem] sm:max-w-[57rem] pt-3 pb-8 gap-y-8 justify-around items-center">
-            {april.map((date) => {
-              if (!attendInfo) return;
-              const time = attendInfo[date];
-              return (
-                <AttendCircle
-                  key={date}
-                  color="border-red-300 bg-red-100/90"
-                  date={date}
-                  time={time}
-                  attend={Boolean(time)}
-                />
-              );
-            })}
+          <div className="path">
+            <div className="mx-auto flex flex-wrap max-w-[17.8rem] sm:max-w-[57rem] pt-3 pb-8 gap-y-8 justify-around items-center">
+              {april.map((date) => {
+                if (!attendInfo) return;
+                const time = attendInfo[date];
+                return (
+                  <AttendCircle
+                    key={date}
+                    color="border-red-300 bg-red-100/90"
+                    date={date}
+                    time={time}
+                    attend={Boolean(time)}
+                  />
+                );
+              })}
+            </div>
           </div>
         </div>
       </div>
